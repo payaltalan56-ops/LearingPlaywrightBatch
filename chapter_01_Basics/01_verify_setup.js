3f2213518cdf6d0b8f55568f28c0f2567015e3b2
rsys-payal-chaudhary <payal.chaudhary@radiansys.com>
2026-04-02 18:13:40 +0530

    Initial commit: add project files and .gitignore

---
+++ chapter_01_Basics/01_verify_setup.js
@@ -0,0 +1,18 @@
+console.log(process.platform);
+//mac - darwin
+//windows - win32
+//linux - linux
+
+
+console.log(process.arch);
+//arm64
+//x64
+
+console.log(process.version);
+//v22.12.0  
+
+console.log(process.execPath);
+// /opt/homebrew/Cellar/node@22/22.12.0/bin/node
+
+console.log(process.cwd());
+// /Users/apple/Documents/LearningPlaywrightBatch
---
+++ chapter_01_Basics/02_hot_code.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(10, 20);
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
